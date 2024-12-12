973772790e2b9c5463c862ffb90ac1c43631b9aa
upoudel888 <upoudel888@gmail.com>
2024-12-12 07:47:04 +0545

    🎁 Add Navbar, Footer and Lora Font

---
+++ src/app/components/ui/Footer.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { GiQuillInk } from "react-icons/gi";
+
+export function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const socialButtonStyles =
+    "rounded-full bg-Gray p-2 text-black ring-1 ring-black transition duration-300 hover:bg-black hover:text-white hover:ring-Blue";
+
+  return (
+    <div className="w-full flex justify-center">
+      <div className="container max-w-screen-2xl px-4 md:px-8 text-center flex flex-col bg-gray-200">
+        <div className="flex flex-col items-center">
+          <div className="logo text-xl flex flex-row gap-2 mt-4">
+            <span className="text-4xl">
+              <GiQuillInk />
+            </span>
+            <span className="leading-10">Iridescent</span>
+          </div>
+          <div className="flex flex-row items-center justify-center gap-4 border-b-[1px] border-black py-4 w-full">
+            <Link href="/" className={`${socialButtonStyles}`}>
+              <FaFacebookF />
+            </Link>
+            <Link href="/" className={`${socialButtonStyles}`}>
+              <FaInstagram />
+            </Link>
+            <Link href="/" className={`${socialButtonStyles}`}>
+              <FaXTwitter />
+            </Link>
+            <Link href="/" className={`${socialButtonStyles}`}>
+              <FaLinkedinIn />
+            </Link>
+          </div>
+        </div>
+        <span className="text-sm py-4">
+          &copy; Ujjwal Paudel {year} | All Rights Reserved
+        </span>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/ui/Navbar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { GiQuillInk } from "react-icons/gi";
+import { MdOutlineLightMode } from "react-icons/md";
+
+export function Navbar() {
+  return (
+    <nav className="w-full h-[4rem] flex justify-center">
+      <div className="container max-w-screen-2xl px-4 md:px-8 flex flex-row items-center justify-between border-b-gray-200 border-b-[1px]">
+        <div className="logo w-1/3 text-xl flex flex-row gap-2">
+          <span className="text-4xl">
+            <GiQuillInk />
+          </span>
+          <span className="leading-10">Iridescent</span>
+        </div>
+        <ul className="w-1/3 flex flex-row gap-8 justify-end items-center text-sm uppercase font-semibold">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Poems</Link>
+          </li>
+        </ul>
+        <div className="mode">
+          <span className="text-2xl cursor-not-allowed">
+            <MdOutlineLightMode />
+          </span>
+        </div>
+      </div>
+    </nav>
+  );
+}
